547f524ec2955696a41e2ce821c9021a38e08cb4
JoanBonell <joanbonellruiz@gmail.com>
2023-04-18 17:23:58 +0200

    Step 6: Communication between View and ViewModel

--- app/src/main/java/cat/udl/hyperion/appmobils/basicmvvm/viewmodel/MyViewModel.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/basicmvvm/viewmodel/MyViewModel.java
@@ -1,7 +1,26 @@
 package cat.udl.hyperion.appmobils.basicmvvm.viewmodel;
 
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import cat.udl.hyperion.appmobils.basicmvvm.model.MyDataModel;
 
 public class MyViewModel extends ViewModel {
-    // Aquí declararás las variables y métodos que serán observados por la vista.
+    // Aquí declararamos las variables y métodos que serán observados por la vista.
+    private MutableLiveData<MyDataModel> myData = new MutableLiveData<>();
+
+    public LiveData<MyDataModel> getMyData() {
+        return myData;
+    }
+
+    public void setMyData(MyDataModel newData) {
+        myData.setValue(newData);
+    }
+
+    // Método para simular la actualización de datos en este ejemplo
+    public void updateMyData(String name, int age) {
+        MyDataModel updatedData = new MyDataModel(name, age);
+        setMyData(updatedData);
+    }
 }
+
