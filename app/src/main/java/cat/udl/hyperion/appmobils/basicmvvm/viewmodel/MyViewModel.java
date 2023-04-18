10945d7a1213584f5a1f9bd6e12e6e351154adc7
JoanBonell <joanbonellruiz@gmail.com>
2023-04-18 16:39:23 +0200

    Step 4: Crear el ViewModel

---
+++ app/src/main/java/cat/udl/hyperion/appmobils/basicmvvm/viewmodel/MyViewModel.java
@@ -0,0 +1,7 @@
+package cat.udl.hyperion.appmobils.basicmvvm.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    // Aquí declararás las variables y métodos que serán observados por la vista.
+}
